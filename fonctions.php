865d1ea3a4018cc61dfc99f0be53f05eb4968671
guillaumepaulhe <guillaumepaulhe@gmail.com>
2022-01-27 19:20:52 +0100

    fix ID btn + classement

--- fonctions.php
+++ fonctions.php
@@ -1,35 +1,48 @@
 <?php
 $db = new PDO('mysql:host=localhost;dbname=caperaa;charset=utf8', 'root', 'root');
 
-function affiche_classement($db){
-	$req_ma_table = $db->prepare("SELECT nom FROM classement ORDER BY 'points' ");
-	$req_ma_table->execute();
-	$result_req_ma_table = $req_ma_table->fetchAll();
-	foreach ($result_req_ma_table as $result) {
-		$res = $result['nom'];
-    }
-}
+// function affiche_classement($db){
+// 	$req_ma_table = $db->prepare("SELECT nom FROM classement ORDER BY 'points' ");
+// 	$req_ma_table->execute();
+// 	$result_req_ma_table = $req_ma_table->fetchAll();
+// 	foreach ($result_req_ma_table as $result) {
+// 		$res = $result['nom'];
+//     }
+// }
 
 function get__classement($db){
-	$req_ma_table = $db->prepare("SELECT * FROM participants");
+	$req_ma_table = $db->prepare("SELECT * FROM participants ORDER BY points DESC");
 	$req_ma_table->execute();
 	$result_req_ma_table = $req_ma_table->fetchAll();
 	$res = '';
+	$i = 0;
 	foreach ($result_req_ma_table as $result) {
+		$i++;
 		$nom = $result['Nom'];
 		$prenom = $result['Prenom'];
-		echo '<li class="classement"> <p class="classement">Nom : '.$nom.'</p><p class="classement">Prenom : '.$prenom.'</p> </li>';
+		$points = $result['points'];
+		if($i==1){
+			echo '<li class="classement"> <p class="classement">🥇'.$i.'</p> <p class="classement">Nom : '.$nom.'</p><p class="classement">Prenom : '.$prenom.'</p> <p class="classement">Points : '.$points.'</p></li>';
+		}
+		if($i==2){
+			echo '<li class="classement"> <p class="classement">🥈'.$i.'</p> <p class="classement">Nom : '.$nom.'</p><p class="classement">Prenom : '.$prenom.'</p> <p class="classement">Points : '.$points.'</p></li>';
+		}
+		if($i==3){
+			echo '<li class="classement"> <p class="classement">🥉'.$i.'</p> <p class="classement">Nom : '.$nom.'</p><p class="classement">Prenom : '.$prenom.'</p> <p class="classement">Points : '.$points.'</p></li>';
+		}
+		if($i >3)
+		echo '<li class="classement"> <p class="classement">'.$i.'</p> <p class="classement">Nom : '.$nom.'</p><p class="classement">Prenom : '.$prenom.'</p> <p class="classement">Points : '.$points.'</p></li>';
 	}
 }
 
-function affiche_participants($db){
-	$req_ma_table = $db->prepare("SELECT Nom FROM participants");
-	$req_ma_table->execute();
-	$result_req_ma_table = $req_ma_table->fetchAll();
-	foreach ($result_req_ma_table as $result) {
-		$res = $result['Nom'];
-	}
-}
+// function affiche_participants($db){
+// 	$req_ma_table = $db->prepare("SELECT Nom FROM participants");
+// 	$req_ma_table->execute();
+// 	$result_req_ma_table = $req_ma_table->fetchAll();
+// 	foreach ($result_req_ma_table as $result) {
+// 		$res = $result['Nom'];
+// 	}
+// }
 
 
 function get__participants($db){
@@ -37,13 +50,15 @@ function get__participants($db){
 	$req_ma_table->execute();
 	$result_req_ma_table = $req_ma_table->fetchAll();
 	$res = '';
+	$i = 0;
 	foreach ($result_req_ma_table as $result) {
 		$nom = $result['Nom'];
 		$prenom = $result['Prenom'];
 		$poids = $result['Poids'];
 		$taille = $result['Taille'];
 		$sexe = $result['Sexe'];
-		echo '<li class="case"> <p class="case">Nom : '.$nom.'</p><p class="case">Prenom : '.$prenom.'</p> <p class="case">Poids : '.$poids." ".' kg</p> <p class="case">Taille : '.$taille.' cm</p> <p class="case">Sexe : '.$sexe.'</p></br><button id="001" class="case">EDIT</button></li>';
+		$id =  $result['idParticipant'];
+		echo '<li class="case"> <p class="case">Nom : '.$nom.'</p><p class="case">Prenom : '.$prenom.'</p> <p class="case">Poids : '.$poids." ".' kg</p> <p class="case">Taille : '.$taille.' cm</p> <p class="case">Sexe : '.$sexe.'</p></br><button id="'.$id.'" class="case">Modifier</button></li>';
 	}
 }
 
@@ -52,9 +67,9 @@ function add_participants($db,$nom,$prenom,$sexe,$age,$taille,$poids){
 	$req_ma_table->execute();
 }
 
-function get_id($db,$nom,$prenom,$poids){
-    $req_ma_table = $db->prepare("SELECT idParticipant FROM `participants` WHERE `nom`='$nom' AND `prenom`='$prenom' AND `poids`='$poids'");
-	$req_ma_table->execute();
-}
+// function get_id($db,$nom,$prenom){
+//     $req_ma_table = $db->prepare("SELECT idParticipant FROM `participants` WHERE `nom`='$nom' AND `prenom`='$prenom' AND `poids`='$poids'");
+// 	$req_ma_table->execute();
+// }
 
     ?>
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 include "base.php";
 ?>
 <div>
-<p>Le challenge Jean-Claude CAPERAA est organisé par le FJEP Lempdes tous les ans le 1er week-end de
+<p class="test">Le challenge Jean-Claude CAPERAA est organisé par le FJEP Lempdes tous les ans le 1er week-end de
 décembre. Il s’agit d’un tournoi de judo pour les benjamins et les poussins. Ce challenge est particulier
 dans la mesure ou le classement des combattants se fait sur 2 critères, l’expression technique (Yaku Soku
 Geiko) et un combat (Randori).
