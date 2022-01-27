4470294f321fddc65b9dfee65074feb0c80012c2
guillaumepaulhe <guillaumepaulhe@gmail.com>
2022-01-27 12:01:35 +0100

    bdd en plus

---
+++ caperaa.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1
+-- http://www.phpmyadmin.net
+--
+-- Client :  localhost
+-- Généré le :  Jeu 27 Janvier 2022 à 11:00
+-- Version du serveur :  5.7.11
+-- Version de PHP :  5.6.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `caperaa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `participants`
+--
+
+CREATE TABLE `participants` (
+  `idParticipant` int(11) NOT NULL,
+  `Nom` varchar(255) NOT NULL,
+  `Prenom` varchar(255) NOT NULL,
+  `Sexe` varchar(5) NOT NULL,
+  `Age` int(255) NOT NULL,
+  `Taille` int(255) NOT NULL,
+  `Poids` int(255) NOT NULL,
+  `Code_club` varchar(4) NOT NULL,
+  `points` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `participants`
+--
+
+INSERT INTO `participants` (`idParticipant`, `Nom`, `Prenom`, `Sexe`, `Age`, `Taille`, `Poids`, `Code_club`, `points`) VALUES
+(7, 'Paulhe', 'Guillaume', 'Homme', 19, 195, 85, '', 3),
+(8, 'Alonso', 'Dorian', 'Homme', 19, 175, 75, '', 5),
+(9, 'Chassaing', 'Arthur', 'Homme', 19, 175, 69, '', 55),
+(10, 'Faucher', 'Noa', 'Homme', 20, 170, 69, '', 0),
+(11, 'Albrecht', 'Sebastien', 'Homme', 19, 180, 81, '', 0),
+(12, 'Ipek', 'Hasan', 'Homme', 19, 175, 58, '', 0),
+(13, 'Faucher', 'Gabriel', 'Homme', 19, 175, 70, '', 0);
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `participants`
+--
+ALTER TABLE `participants`
+  ADD PRIMARY KEY (`idParticipant`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `participants`
+--
+ALTER TABLE `participants`
+  MODIFY `idParticipant` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
