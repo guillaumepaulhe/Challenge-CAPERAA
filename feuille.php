6adad018f14d16eaa1128d50cecb243ca875f139
guillaumepaulhe <guillaumepaulhe@gmail.com>
2022-03-22 11:22:58 +0100

    poule de 3 OK

--- feuille.php
+++ feuille.php
@@ -2,61 +2,60 @@
 include "base.php";
 ?>
 
-<div class="feuille"> 
+<div class="grid-container">
+<div class="grid-text"> 
 <p class="text_feuille">A1 : Alonso Dorian</p>
-<br>
 <p class="text_feuille">A2 : Paulhe Guillaume</p>
-<br>
 <p class="text_feuille">A3 : Chassaing Arthur</p>
-<br>
 </div>
 
-<div class="feuille">
-<div class="grid-btn">
-<input class="grid-btn" id="noir"></input>
-<input type="number" class="grid-btn"></input>
-<input type="number" class="grid-btn"></input>
-<input type="number" class="grid-btn"></input>
-<input type="number" class="grid-btn"></input>
-<input class="grid-btn" id="noir"></input>
-<input type="number" class="grid-btn"></input>
-<input type="number" class="grid-btn"></input>
-<input type="number" class="grid-btn"></input>
-<input type="number" class="grid-btn"></input>
-<input class="grid-btn"id="noir"></input>
-<input type="number" class="grid-btn"></input>
+<div class="cases">
+
+<div class="case-1">
+<input class="grid-btn" id="noir" disabled></input>
+<input type="number" class="grid-btn" min=1 max=10></input>
+<input type="number" class="grid-btn" min=1 max=10></input>
+<input type="number" class="grid-btn" min=1 max=10></input>
+<input type="number" class="grid-btn" min=1 max=10></input>
+<input class="grid-btn" id="noir" disabled></input>
+<input type="number" class="grid-btn" min=1 max=10></input>
+<input type="number" class="grid-btn" min=1 max=10></input>
+<input type="number" class="grid-btn" min=1 max=10></input>
+<input type="number" class="grid-btn" min=1 max=10></input>
+<input class="grid-btn"id="noir" disabled></input>
+<input type="number" class="grid-btn" min=1 max=10></input>
 </div>
 
-<div class="grid-btn2">
-<input class="grid-btn" id="noir"></input>
-<input type="number" class="grid-btn"></input>
-<input type="number" class="grid-btn"></input>
-<input type="number" class="grid-btn" id="noir"></input>
-<input type="number" class="grid-btn" id="noir"></input>
-<input class="grid-btn" id="noir"></input>
-<input type="number" class="grid-btn"></input>
-<input type="number" class="grid-btn" id="noir"></input>
-<input type="number" class="grid-btn" ></input>
-<input type="number" class="grid-btn" id="noir"></input>
-<input class="grid-btn" id="noir"></input>
-<input type="number" class="grid-btn" id="noir"></input>
-<input class="grid-btn"></input>
-<input class="grid-btn"></input>
-<input class="grid-btn" id="noir"></input>
-<input class="grid-btn" id="noir"></input>
-<input class="grid-btn" id="noir"></input>
-<input class="grid-btn" id="noir"></input>
+<div class="case-2">
+<input class="grid-btn" id="noir" disabled ></input>
+<input type="number" class="grid-btn" min=1 max=10></input>
+<input type="number" class="grid-btn" min=1 max=10></input>
+<input class="grid-btn" id="noir" disabled></input>
+<input class="grid-btn" id="noir" disabled></input>
+<input class="grid-btn" id="noir" disabled></input>
+<input type="number" class="grid-btn" min=1 max=10></input>
+<input class="grid-btn" id="noir" disabled></input>
+<input type="number" class="grid-btn" min=1 max=10 ></input>
+<input class="grid-btn" id="noir" disabled></input>
+<input class="grid-btn" id="noir" disabled></input>
+<input class="grid-btn" id="noir" disabled></input>
+<input type="number" class="grid-btn" min=1 max=10></input>
+<input type="number" class="grid-btn" min=1 max=10></input>
+<input class="grid-btn" id="noir" disabled></input>
+<input class="grid-btn" id="noir" disabled></input>
+<input class="grid-btn" id="noir" disabled></input>
+<input class="grid-btn" id="noir" disabled></input>
 </div>
 
-<div class="grid-btn3">
-<input type="number" class="grid-btn"></input>
-<input type="number" class="grid-btn"></input>
-<input type="number" class="grid-btn"></input>
-<input type="number" class="grid-btn"></input>
-<input type="number" class="grid-btn"></input>
-<input type="number" class="grid-btn"></input>
-<input type="number" class="grid-btn"></input>
-<input type="number" class="grid-btn"></input>
-<input type="number" class="grid-btn"></input>
+<div class="case-3">
+<input type="number" class="grid-btn" min=1 max=10></input>
+<input type="number" class="grid-btn" min=1 max=10></input>
+<input type="number" class="grid-btn" min=1 max=10></input>
+<input type="number" class="grid-btn" min=1 max=10></input>
+<input type="number" class="grid-btn" min=1 max=10></input>
+<input type="number" class="grid-btn" min=1 max=10></input>
+<input type="number" class="grid-btn" min=1 max=10></input>
+<input type="number" class="grid-btn" min=1 max=10></input>
+<input type="number" class="grid-btn" min=1 max=10></input>
 </div>
 </div>
